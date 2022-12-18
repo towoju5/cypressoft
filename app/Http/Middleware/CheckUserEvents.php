c24c0f4273170d1f7bdf5e1b8b642e7895c2b2e5
EMMANUEL TOWOJU <towojuads@gmail.com>
2022-12-18 23:43:55 +0100

    commit

--- app/Http/Middleware/CheckUserEvents.php
+++ app/Http/Middleware/CheckUserEvents.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Events;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,12 +18,6 @@ class CheckUserEvents
      */
     public function handle(Request $request, Closure $next)
     {
-        return response()->json($request);
-        if($request->has('event_id') && $request->has('user_id')){
-            if(Route::is('get.events')){
-                // check if user event is edited. and get events.
-            }
-        }
         return $next($request);
     }
 }
