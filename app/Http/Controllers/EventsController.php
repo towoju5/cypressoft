de9a32c3e06c62f6ce67f6fcdc8592214ff6531b
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2022-12-19 11:30:31 +0100

    Update EventsController.php

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Events;
-use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class EventsController extends Controller
 {
@@ -16,11 +14,40 @@ class EventsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $events = Events::all()->makeHidden(['user_id', 'created_at', 'updated_at']);
-        $users  = User::all();
-        return view('events.create', compact(['events', 'users']));
+        try {
+            if ($request->has('date_from') && !empty($request->date_from)) :
+                if ($request->date_from > $request->date_to) {
+                    $response = get_error_response(400, 'Invalid request', ['error' => 'Date from should be earlier']);
+                    return response()->json($response, 400);
+                }
+                
+                $date_from = Carbon::parse($request->date_from);
+
+                if($request->has('date_to') && !empty($request->date_to)):
+                    $date_to = Carbon::parse($request->date_to)->addDay();
+                else :
+                    $date_to = Carbon::now()->addDay();
+                endif;
+
+                $events = Events::groupBy('slug')
+                    ->whereBetween('created_at', [$date_from, $date_to])
+                    ->get()
+                    ->makeHidden(['created_at', 'updated_at', 'user_id', 'description'])
+                    ->toArray();
+                $response = get_success_response($events);
+                return response()->json($response, 200);
+            endif;
+
+            $events = Events::paginate(10)->toArray();
+            return response()->json($events);
+
+        } catch (\Throwable $th) {
+            return get_error_response($th->getCode(), $th->getMessage(), [
+                'error' => $th->getMessage(),
+            ]);
+        }
     }
 
     /**
@@ -28,11 +55,22 @@ class EventsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function list()
+    public function filter(Request $request)
     {
-        $events = Events::all()->makeHidden(['created_at', 'updated_at']);
-        $users  = User::all();
-        return view('events.list', compact(['events', 'users']));
+        try {
+            $date_from = Carbon::parse($request->date_from);
+            $date_to = Carbon::parse($request->date_to)->addDay();
+            $events = Events::where('user_id', auth('sanctum')->id())
+                ->whereBetween('created_at', [$date_from, $date_to])
+                ->get()
+                ->makeHidden(['created_at', 'updated_at', 'user_id'])
+                ->toArray();
+            return get_success_response($events);
+        } catch (\Throwable $th) {
+            return get_error_response($th->getCode(), $th->getMessage(), [
+                'error' => $th->getMessage(),
+            ]);
+        }
     }
 
     /**
@@ -53,51 +91,31 @@ class EventsController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'event_title'           =>  'required',
-            'event_image'           =>  'required',
-            'event_start'           =>  'required',
-            'event_description'     =>  'required',
-        ]);
-
-        // check limits
-        $events = Events::where('created_at', Carbon::today())->count();
-        if ($events >= 4) {
-            return back()->with('error', "Event limit reached for today");
-        }
-        $event = new Events();
-        $event->title         = $request->event_title;
-        $event->user_id       = $request->user_id;
-        $event->slug          = Str::slug($request->event_title);
-        $event->start         = $request->event_start;
-        $event->end           = $request->event_end;
-        $event->image         = save_image('events', $request->event_image);
-        $event->description   = $request->event_description;
-        if ($event->save()) :
-            return back()->with('success', "Event added successfully");
-        else :
-            return back()->with('error', "Error encountered while saving event");
-        endif;
+        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
     public function show(Request $request, $slug)
     {
-        $event = Events::where('slug', $slug)->count();
-        if ($event < 1) :
+        $event = Events::where('slug', $slug)->get();
+
+        if(empty($event)) :
             $result = get_error_response(404, "Event not found",  ["error" => "Event not found"]);
-            return response()->json($result, 404);
-        elseif ($event > 0) :
-            $event = Events::where(['user_id', $request->user->id])->where('slug', $slug)->first();
+            return response()->json($result , 404);
+        endif;
+
+
+        $event = Events::where(['user_id' => auth('sanctum')->id()])->where('slug', $slug)->first()->makeHidden(['user_id', 'updated_at'])->toArray();
+        if(!empty($event)) :
             $result = get_success_response($event);
-            return response()->json($result, 200);
+            return response()->json($result , 200);
         else :
-            $event = Events::where('user_id', 0)->where('slug', $slug)->first();
+            $event = Events::where('user_id', 0)->where('slug', $slug)->first()->makeHidden(['user_id', 'updated_at'])->toArray();
             $result = get_success_response($event);
             return response()->json($result, 200);
         endif;
@@ -111,9 +129,7 @@ class EventsController extends Controller
      */
     public function edit($id)
     {
-        $event = Events::findorFail($id);
-        $users  = User::all();
-        return view('events.edit', compact(['event', 'users']));
+        //
     }
 
     /**
@@ -125,40 +141,7 @@ class EventsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'event_title'           =>  'required',
-            'event_start'           =>  'required',
-            'event_description'     =>  'required',
-        ]);
-
-        if ($request->user_id != 0) :
-            $check_event = Events::where('id', $id)->where('user_id', $request->user_id)->count();
-            if ($check_event < 1) :
-                // create event for user
-                if ($this->store($request)) :
-                    return back()->with('success', "Event updated successfully");
-                else :
-                    return back()->with('error', "Error encountered while updating event");
-                endif;
-            endif;
-        endif;
-
-        $event = Events::findorFail($id);
-        $event->title         = $request->event_title;
-        $event->user_id       = $request->user_id;
-        $event->slug          = Str::slug($request->event_title);
-        $event->start         = $request->event_start;
-        $event->end           = $request->event_end;
-        if ($request->has('event_image')) :
-            $event->image     = save_image('events', $request->event_image);
-        endif;
-        $event->description   = $request->event_description;
-
-        if ($event->save()) :
-            return back()->with('success', "Event updated successfully");
-        else :
-            return back()->with('error', "Error encountered while updating event");
-        endif;
+        //
     }
 
     /**
@@ -169,10 +152,6 @@ class EventsController extends Controller
      */
     public function destroy($id)
     {
-        if (Events::destroy([$id])) :
-            return back()->with('success', "Event deleted successfully");
-        else :
-            return back()->with('error', "Unable to delete event");
-        endif;
+        //
     }
 }
